424b876565e4ae45890eda049635fb94b298793a
Ihor Moroz <shangriladida2@gmail.com>
2026-03-31 12:44:53 +0300

    feat: first telegram bot response

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const db = require('./db');
 
 db.run(
-	"INSERT INTO test (name) VALUES (?)", 
+	"INSERT INTO test (name) VALUES (?)",
 	['Ihor'],
 
 	function (err) {
 		if (err) {
 			console.error(err);
 			return;
-		} 
+		}
 
 
 		db.all(`SELECT * FROM test`, [], (err, rows) => {
-			console.log('Before update: ', rows );
+			console.log('Before update: ', rows);
 
 
-			db.run('UPDATE test SET name = ? WHERE id = ?', 
+			db.run('UPDATE test SET name = ? WHERE id = ?',
 				['Piter', this.lastID],
 				function (err) {
-				if (err) {
-					console.error(err);
-					return;
+					if (err) {
+						console.error(err);
+						return;
 					}
-					
-					
+
+
 					db.all(`SELECT * FROM test`, [], (err, rows) => {
 						console.log('After update:', rows);
 					});
@@ -31,4 +31,22 @@ db.run(
 			);
 		});
 	}
-);			
+);
+
+
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('8681129054:AAGiRSq_A3TIfIdN51qo0STZC3YN7CEcaJk');
+
+bot.start((ctx) => {
+	ctx.reply('Привіт! Я працюю 🤖');
+});
+
+bot.on('text', (ctx) => {
+	ctx.reply(`Ти написав: ${ctx.message.text}`);
+});
+
+bot.launch();
+
+console.log('Bot started');
