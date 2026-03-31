174f631c5f1e886edbcaaf76963c2f708e09aecb
Ihor Moroz <shangriladida2@gmail.com>
2026-03-31 16:13:25 +0300

    refactor: replace test table with real order structure

--- db.js
+++ db.js
@@ -4,11 +4,46 @@ const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
 	db.run(`
-    CREATE TABLE IF NOT EXISTS test (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT
-    )
-  `);
+		CREATE TABLE IF NOT EXISTS days (
+      	id INTEGER PRIMARY KEY AUTOINCREMENT,
+      	name TEXT
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS places (
+      	id INTEGER PRIMARY KEY AUTOINCREMENT,
+      	date TEXT
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS items (
+      	id INTEGER PRIMARY KEY AUTOINCREMENT,
+      	place_id INTEGER,
+			name TEXT,
+			default_quantity INTEGER
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS day_items (
+      	id INTEGER PRIMARY KEY AUTOINCREMENT,
+      	day_id INTEGER,
+			item_id INTEGER,
+			quantity INTEGER
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS delivery (
+      	id INTEGER PRIMARY KEY AUTOINCREMENT,
+      	day_id INTEGER,
+			name TEXT,
+			comment TEXT
+		)
+	`);
+
 });
 
 module.exports = db;
