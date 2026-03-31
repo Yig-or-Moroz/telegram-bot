222fe422da6361695523be24cc3db9760e13ef1c
Ihor Moroz <shangriladida2@gmail.com>
2026-03-31 11:39:24 +0300

    feat: insert and read data from sqlite

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const db = require('./db');
 
-console.log("DB connected");
+db.run(
+	"INSERT INTO test (name) VALUES (?)", 
+	['Ihor'],
+
+	function (err) {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log("Inserted row with id:" , this.lastID)
+		}
+	} 
+);
+
+
+db.all(`SELECT * FROM test`, [], (err, rows) => {
+	console.log('All rows: ', rows );
+});
