821a1cf0d759d0b91e6c2d25448360bbc91b1a23
Ihor Moroz <shangriladida2@gmail.com>
2026-03-31 13:21:41 +0300

    feat: read data from sqlite and show in telegram

--- index.js
+++ index.js
@@ -1,50 +1,23 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
 const db = require('./db');
 
-db.run(
-	"INSERT INTO test (name) VALUES (?)",
-	['Ihor'],
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-	function (err) {
+bot.start((ctx) => {
+	db.all(`SELECT * FROM test`, [], (err, rows) => {
 		if (err) {
-			console.error(err);
+			ctx.reply('DB error');
 			return;
 		}
 
-
-		db.all(`SELECT * FROM test`, [], (err, rows) => {
-			console.log('Before update: ', rows);
-
-
-			db.run('UPDATE test SET name = ? WHERE id = ?',
-				['Piter', this.lastID],
-				function (err) {
-					if (err) {
-						console.error(err);
-						return;
-					}
-
-
-					db.all(`SELECT * FROM test`, [], (err, rows) => {
-						console.log('After update:', rows);
-					});
-				}
-			);
+		let message = 'Дані з бази:\n\n';
+		rows.forEach((row) => {
+			message += `${row.id}. ${row.name}\n`;
 		});
-	}
-);
 
-
-
-const { Telegraf } = require('telegraf');
-
-const bot = new Telegraf('8681129054:AAGiRSq_A3TIfIdN51qo0STZC3YN7CEcaJk');
-
-bot.start((ctx) => {
-	ctx.reply('Привіт! Я працюю 🤖');
-});
-
-bot.on('text', (ctx) => {
-	ctx.reply(`Ти написав: ${ctx.message.text}`);
+		ctx.reply(message);
+	});
 });
 
 bot.launch();
